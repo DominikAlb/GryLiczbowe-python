20bfb6afd15206bf4743ff68fa95d86e44ff1caa
DominikAlb <dominik.albiniak@o2.pl>
2022-04-12 10:50:02 +0200

    fix multimulti

--- games/multi.py
+++ games/multi.py
@@ -1,16 +1,49 @@
 import random
 import string
 import logging
+import statistics as stat
 
-from games.euroRoulette import EuroRoulette
+from games.game import Game
 
 
-class MultiMulti(EuroRoulette):
-    def __init__(self, name: string, debug: bool, min_val: int, max_val: int, randomInput: bool, *args):
-        super().__init__(name, debug, min_val, max_val, randomInput, *args)
+class MultiMulti(Game):
+    def __init__(self, name: string, debug: bool, min_val: int, max_val: int, *args):
+        super().__init__(name, debug, min_val, max_val, *args)
 
     def monteCarlo(self, numSpins: int) -> float:
-        return super().monteCarlo(numSpins)
+        wins: int = 0
+        numbers = random.sample(range(self.min_val, self.max_val), self.n)
+        for i in range(0, numSpins):
+            if super().draw(self.min_val, self.max_val, numbers, self.m):
+                wins = wins + 1
+            self.games.append(i + 1)
+            self.gameResults.append(wins / (i + 1))
+        super().draft()
+        if self.debug:
+            logging.info(
+                "Oczekiwany wynik wylosowania w " + self.name + ": " + str(numbers) + " , to: " + str(100 * wins / numSpins) + "%\n")
+            logging.info(
+                "liczba wygranych: " + str(wins) + "\n\n")
+        return wins / numSpins
 
     def lasVegas(self, numSpins: int) -> float:
-        return super().lasVegas(numSpins)
+        count: int = 0
+        numbers = []
+        if not self.randomInput:
+            numbers = random.sample(range(self.min_val, self.max_val), self.n)
+        for i in range(0, numSpins):
+            while True:
+                if self.randomInput:
+                    numbers = random.sample(range(self.min_val, self.max_val), self.n)
+                count += 1
+                if super().draw(self.min_val, self.max_val, numbers, self.m):
+                    break
+            self.games.append(i + 1)
+            self.gameResults.append(count)
+        super().draft()
+        if self.debug:
+            logging.info(
+                "Oczekiwany sredni czas wygranej w " + self.name + ": " + str(numbers) + " , to: " + str(
+                    stat.mean(self.gameResults)) + "%\n")
+
+        return stat.mean(self.gameResults)
